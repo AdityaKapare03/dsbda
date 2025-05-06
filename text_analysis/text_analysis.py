e7916702aecfdf29fbabaeb0d5b90717dba18a86
Celestial <adityakapare@proton.me>
2025-05-06 18:15:15 +0530

    Added new files

---
+++ text_analysis/text_analysis.py
@@ -0,0 +1,83 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.17.1
+#   kernelspec:
+#     display_name: Python 3 (ipykernel)
+#     language: python
+#     name: python3
+# ---
+
+# %%
+import nltk
+
+# %%
+nltk.download('punkt')
+nltk.download('punkt_tab')
+nltk.download('stopwords')
+nltk.download('averaged_perceptron_tagger_eng')
+nltk.download('wordnet')
+
+# %%
+import pandas as pd
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+from nltk.stem import PorterStemmer, WordNetLemmatizer
+from nltk import pos_tag
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+# %%
+document = 'Text analysis is the process of extracting meaningful insights from textual data. It involves techniques like tokenization, stemming and lemmatization.'
+
+# %%
+tokens = word_tokenize(document)
+print('Tokens: ', tokens)
+
+# %%
+pos_tags = pos_tag(tokens)
+print(pos_tags)
+
+# %%
+stop_words = set(stopwords.words('english'))
+filtered_tokens = [word for word in tokens if word.lower() not in stop_words]
+print('After stop words removal: ', filtered_words)
+
+# %%
+stemmer = PorterStemmer()
+stemmed_tokens = [stemmer.stem(word) for word in filtered_tokens]
+print('Stemmed Tokens', stemmed_tokens)
+
+# %%
+lemmatizer = WordNetLemmatizer()
+lemmatized_tokens = [lemmatizer.lemmatize(word) for word in stemmed_tokens]
+print(lemmatized_tokens)
+
+# %%
+text = " ".join(lemmatized_tokens)
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+wordcloud = WordCloud(width = 800, height = 400, background_color = 'white').generate(text)
+plt.figure(figsize=(10, 5))
+plt.imshow(wordcloud, interpolation='bilinear')
+plt.axis('off')
+plt.show()
+
+# %%
+documents = [
+    'Text analysis is the process of extracting meaningful insights from textual data.',
+    'It involves techniques like tokenization, stemming and lemmatization.',
+    'It provides summary or extract of the text.'
+]
+
+# %%
+vectorizer = TfidfVectorizer()
+tfidf_matrix = vectorizer.fit_transform(documents)
+feature_names = vectorizer.get_feature_names_out()
+
+print('TF IDF matrix:')
+print(tfidf_matrix.toarray())
+print('Terms:', feature_names)
